14447f3c1fa11f81bd0ef3bd6a49200c9479d791
Jérôme STEUNOU <jerome.steunou@steeple.com>
2024-12-20 11:15:12 +0100

    revert: do not specify name

--- bin/image-optimization.ts
+++ bin/image-optimization.ts
@@ -7,7 +7,6 @@ import { REC_REGIONS } from '../lib/origin-shield'
 
 const app = new cdk.App()
 new ImageOptimizationStack(app, 'ImgTransformationStack', {
-    stackName: 'image-optimization-stack-poc',
     description: 'Stack with CF + S3 + Lambda to handle image hosting & resize',
     env: {
         region: REC_REGIONS.EU_WEST_1
